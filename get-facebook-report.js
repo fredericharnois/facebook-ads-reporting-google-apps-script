f6dbcc30bc75bbaca990d4e15e9e3dac9ed1bf6e
Frederic Harnois <fredericharnois@users.noreply.github.com>
2020-02-26 21:27:12 -0500

    Updated to Graph API v6.0 and reworked formatting

--- get-facebook-report.js
+++ get-facebook-report.js
@@ -4,7 +4,7 @@
 * 
 * Pushes the Facebook asynchronous report to Google Sheets
 *
-* Version: 2.1
+* Version: 2.2
 *
 * Google Apps Script maintained by Frederic Harnois
 * fred@fredericharnois.com
@@ -14,37 +14,30 @@
 // MODIFY YOUR SETTINGS HERE //
 
 // url of the google sheets where the report will be
-var SPREADSHEET_URL = 'INSERT_URL'
+const SPREADSHEET_URL = 'INSERT_URL'
 
 // name of the sheet where the report will be
-var TAB_NAME = 'INSERT_TAB_NAME'
-
-// user access token linked to a Facebook app
-var TOKEN = 'INSERT_TOKEN'
+const TAB_NAME = 'INSERT_TAB_NAME'
 
 // DO NOT MODIFY ANYTHING BELOW //
 
 function getFacebookReport() {
   
   // Selects the chosen sheet and tab
-  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
-  var sheet = ss.getSheetByName(TAB_NAME);
+  const ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  const sheet = ss.getSheetByName(TAB_NAME);
   
   // Clears the sheet
   sheet.clear();
   
   // Gets the Facebook report run ID
-  var cache = CacheService.getScriptCache();
-  var reportId = cache.get('campaign-report-id')
+  const cache = CacheService.getScriptCache();
+  const reportId = cache.get('campaign-report-id');
 
   // Fetches the report as a csv file
-  var url = 
-      'https://www.facebook.com/ads/ads_insights/export_report' +
-      '?report_run_id=' + reportId +
-      '&format=csv' +
-      '&access_token=' + TOKEN;
-  var fetchRequest = UrlFetchApp.fetch(url);
-  var results = Utilities.parseCsv(fetchRequest);
+  const url = `https://www.facebook.com/ads/ads_insights/export_report?report_run_id=${reportId}&format=csv&access_token=${TOKEN}`;
+  const fetchRequest = UrlFetchApp.fetch(url);
+  const results = Utilities.parseCsv(fetchRequest);
   
   // Pastes the csv file in the sheet
   sheet.getRange(1,1, results.length, results[0].length).setValues(results);
--- request-facebook-report.js
+++ request-facebook-report.js
@@ -4,7 +4,7 @@
 * 
 * Requests a Facebook report asynchronously and caches the report ID
 *
-* Version: 2.1
+* Version: 2.2
 *
 * Google Apps Script maintained by Frederic Harnois
 * fred@fredericharnois.com
@@ -14,62 +14,54 @@
 // MODIFY YOUR REPORT HERE //
 
 // ad account ID
-var AD_ACCOUNT_ID = 'INSERT_AD_ACCOUNT_ID'
+const AD_ACCOUNT_ID = 'INSERT_AD_ACCOUNT_ID'
 
 // ad, adset, campaign, account
-var LEVEL = 'INSERT_LEVEL'
+const LEVEL = 'INSERT_LEVEL'
 
 // https://developers.facebook.com/docs/marketing-api/insights/parameters#fields
-var FIELDS = 'INSERT_FIELDS'
+const FIELDS = 'INSERT_FIELDS'
 
 // https://developers.facebook.com/docs/marketing-api/insights/parameters#param
-var DATE_RANGE = 'INSERT_DATE_RANGE'
+const DATE_RANGE = 'INSERT_DATE_RANGE'
 
 // user access token linked to a Facebook app
-var TOKEN = 'INSERT_TOKEN'
+const TOKEN = 'INSERT_TOKEN'
 
 // number of days from 1 to 90
-var TIME_INCREMENT = 'INSERT_TIME_INCREMENT'
+const TIME_INCREMENT = 'INSERT_TIME_INCREMENT'
 
 // https://developers.facebook.com/docs/marketing-api/insights/parameters#param
-var FILTERING = 'INSERT_FILTERS'
+const FILTERING = 'INSERT_FILTERS'
 
 // DO NOT MODIFY ANYTHING BELOW //
 
 function requestFacebookReport() {
 
   // Builds the Facebook Ads Insights API URL
-  var facebookUrl = 
-    'https://graph.facebook.com/v5.0' + 
-    '/act_' + AD_ACCOUNT_ID +
-    '/insights?level=' + LEVEL +
-    '&fields=' + FIELDS +
-    '&date_preset=' + DATE_RANGE +
-    '&access_token=' + TOKEN +
-    '&time_increment=' + TIME_INCREMENT +
-    '&filtering=' + FILTERING +
-    '&limit=1000';
-  var encodedFacebookUrl = encodeURI(facebookUrl);
-  var options = {
+  const facebookUrl = `https://graph.facebook.com/v6.0/act_${AD_ACCOUNT_ID}/insights?level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_RANGE}&access_token=${TOKEN}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&limit=1000`;
+  const encodedFacebookUrl = encodeURI(facebookUrl);
+  
+  const options = {
     'method' : 'post'
   };
   
   // Fetches & parses the URL 
-  var fetchRequest = UrlFetchApp.fetch(encodedFacebookUrl, options);
-  var results = JSON.parse(fetchRequest.getContentText());
+  const fetchRequest = UrlFetchApp.fetch(encodedFacebookUrl, options);
+  const results = JSON.parse(fetchRequest.getContentText());
   
   // Caches the report run ID
-  var reportId = results.report_run_id;
-  var cache = CacheService.getScriptCache();
-  var cached = cache.get("campaign-report-id");
+  const reportId = results.report_run_id;
+  const cache = CacheService.getScriptCache();
+  const cached = cache.get('campaign-report-id');
   
   if (cached != null) {
-    cache.put("campaign-report-id", [], 1);
+    cache.put('campaign-report-id', [], 1);
     Utilities.sleep(1001);
-    cache.put("campaign-report-id", reportId, 21600);
-    }
-  else {
-    cache.put("campaign-report-id", reportId, 21600); 
-    }
+    cache.put('campaign-report-id', reportId, 21600);
+  } else {
+    cache.put('campaign-report-id', reportId, 21600); 
+  };
+  
   Logger.log(cache.get('campaign-report-id'));
 }
